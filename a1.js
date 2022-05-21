103a4ff0ac2a6cc7499eb74a8e64fa4e76948b98
Pmalinnag <PMlinnag039319@gmail.com>
2022-05-20 21:43:54 -0400

    First Commit

---
+++ a1.js
@@ -0,0 +1,47 @@
+
+/*********************************************************************************
+*  WEB700 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Precious Marie E. Malinnag Student ID: 147010219 Date: May 20, 2022
+*
+********************************************************************************/ 
+
+console.log("Hello World")
+
+//variable 1
+var serverVerbs = ["GET", "GET", "GET", "POST", "GET", "POST"]
+console.log(serverVerbs) //print the array in variable 1
+console.log(serverVerbs[0])
+console.log(serverVerbs[1])
+console.log(serverVerbs[2])
+console.log(serverVerbs[3])
+console.log(serverVerbs[4])
+console.log(serverVerbs[5])
+
+//variable 2
+var serverPaths = [ "/", "/about", "/contact", "/login", "/panel", "/logout"]
+console.log(serverPaths) //print the array in variable 2
+console.log(serverPaths[0])
+console.log(serverPaths[1])
+console.log(serverPaths[2])
+console.log(serverPaths[3])
+console.log(serverPaths[4])
+console.log(serverPaths[5])
+
+//variable 3
+var serverResponse = ["Welcome to WEB700 Assignment 1", "This assignment was prepared by Precious Marie Malinnag", 
+"Student Name: Precious Marie Malinnag Student Email: pmalinnag@myseneca.ca", "User Looged In", "Main Panel", "Logout Complete"]
+console.log(serverResponse) //print the array in variable 3
+console.log(serverResponse[0])
+console.log(serverResponse[1])
+console.log(serverResponse[2])
+console.log(serverResponse[3])
+console.log(serverResponse[4])
+console.log(serverResponse[5])
+
+// The web server simulator function
+
+
